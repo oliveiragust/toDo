d870813895618c3a89c516fc92a41de77a5fe3a0
Casa <novousuario@Casa.local>
2024-03-16 17:23:21 -0300

    final

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
     task = StringField('Tarefa', validators=[DataRequired()])
-    task_description = StringField('Descriçã)
+    task_description = StringField('Descrição')
     task_status = BooleanField('Task Completion', default=False)
     submit = SubmitField('Submit')
     atualizar = SubmitField('Complete')
